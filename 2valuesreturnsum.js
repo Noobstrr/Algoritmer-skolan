d32e9f28c3d55160f34222438b870bde021557ac
Noobstrr <Fabian.malmstrom@gmail.com>
2023-01-24 10:37:40 +0100

    1 done

---
+++ 2valuesreturnsum.js
@@ -0,0 +1,4 @@
+function plus(val1, val2){ //skapar funktion som tar emot 2 värden
+    return val1+val2; //plussar ihop talen och sedan skickar tillbaka dem
+}
+console.log(plus(1,3)); // loggar det svar funktionen skickar tillbaka
